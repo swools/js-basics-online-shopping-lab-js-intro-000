973f700094f8c469f95be73978df8e2ae3f66555
Stephen Wool <stevewool7@gmail.com>
2019-03-05 15:39:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -42,8 +42,7 @@ function total() {
 function removeFromCart(item) {
   for (var i = 0; i < cart.length; i++) {
     if (cart[i].itemName == item) {
-      return item
-      //cart.splice(i, 1)
+      cart.splice(i, 1)
     }
   }
 }
