ddb29c343af8e2ce1259fcac94a1bca4a6d9fbcf
Stephen Wool <stevewool7@gmail.com>
2019-03-05 16:33:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
-  if (cardNumber == null) {
+  if (cardNumber === null) {
     return console.log("Sorry, we don't have a credit card on file for you.")
   }
 
