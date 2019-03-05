dbbb924502b54679e1f6e662cd545baa905ecb32
Stephen Wool <stevewool7@gmail.com>
2019-03-05 16:29:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -55,5 +55,7 @@ function placeOrder(cardNumber) {
     } else {
       return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
     }
-    cart = [];
+    function emptyCart() {
+      cart = [];
+    }
 }
