891c5e9c966e14b87d8cab3abeccf9dd2189afc0
Stephen Wool <stevewool7@gmail.com>
2019-03-05 16:34:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -50,16 +50,14 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-    function emptyCart() {
-      cart = [];
-    }
+
 
     if(!cardNumber) {
       return `Sorry, we don't have a credit card on file for you.`
     } else {
       return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
     }
-    emptyCart();
+
 }
 
 
