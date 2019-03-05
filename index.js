6e631e69376a96e3bac30f3387ff92a60fa64547
Stephen Wool <stevewool7@gmail.com>
2019-03-05 15:01:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- cart.push({ itemName: item, itemPrice: Math.floor((Math.random() * (100 - 1) + 1 ))})
- return `${item} has been added to your cart.`
+ cart.push({ itemName: item, itemPrice: Math.floor((Math.random() * (100 - 1) + 1 ))});
+ return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
